f39721ca1ab5a1dfd11d0b00993c89e72f340429
Son Juwan <wndhks171@naver.com>
2025-08-30 05:03:01 +0900

    setting/#3: Home 뷰 + 네비게이션

---
+++ app/src/main/java/org/ssg_tab/presentation/ui/home/navigation/HomeNavigation.kt
@@ -0,0 +1,29 @@
+package org.ssg_tab.presentation.ui.home.navigation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.SnackbarHostState
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import kotlinx.serialization.Serializable
+import org.ssg_tab.core.navigation.MainTabRoute
+import org.ssg_tab.presentation.ui.home.HomeScreen
+
+fun NavController.navigateToHome(
+    navOptions: NavOptions? = null
+) {
+    navigate(Home, navOptions)
+}
+
+fun NavGraphBuilder.homeNavigation(
+    paddingValues: PaddingValues,
+    snackbarHostState: SnackbarHostState
+) {
+    composable<Home> {
+        HomeScreen()
+    }
+}
+
+@Serializable
+data object Home : MainTabRoute
