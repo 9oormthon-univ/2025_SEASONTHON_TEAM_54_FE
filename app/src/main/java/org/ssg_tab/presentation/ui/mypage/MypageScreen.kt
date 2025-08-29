0fc2e7db28fc86900819502d1965cb9633b9c240
Son Juwan <wndhks171@naver.com>
2025-08-30 05:03:10 +0900

    setting/#3: Mypage 뷰 + 네비게이션

---
+++ app/src/main/java/org/ssg_tab/presentation/ui/mypage/MypageScreen.kt
@@ -0,0 +1,11 @@
+package org.ssg_tab.presentation.ui.mypage
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MypageScreen (
+
+) {
+    Text("마이페이지 화면")
+}
