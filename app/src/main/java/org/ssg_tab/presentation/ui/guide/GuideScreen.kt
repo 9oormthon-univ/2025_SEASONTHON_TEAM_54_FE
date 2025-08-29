9185d3ea07572ea6a547904d9bba1235733fd0a5
Son Juwan <wndhks171@naver.com>
2025-08-30 05:02:51 +0900

    setting/#3: Guide 뷰 + 네비게이션

---
+++ app/src/main/java/org/ssg_tab/presentation/ui/guide/GuideScreen.kt
@@ -0,0 +1,11 @@
+package org.ssg_tab.presentation.ui.guide
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun GuideScreen (
+
+) {
+    Text("가이드 화면")
+}
