2c1fa3e13f80721f6413e58e642a87c25a8ffe5a
Son Juwan <wndhks171@naver.com>
2025-08-30 20:47:19 +0900

    feat/#9: Login dummy View + Navigation

--- app/src/main/java/org/ssg_tab/presentation/ui/login/LoginScreen.kt
+++ app/src/main/java/org/ssg_tab/presentation/ui/login/LoginScreen.kt
@@ -1,9 +1,39 @@
 package org.ssg_tab.presentation.ui.login
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import org.ssg_tab.core.util.noRippleClickable
 
 @Composable
-fun LoginScreen (
-
+fun LoginScreen(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
 ) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(
+                color = Color.White
+            )
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp, vertical = 16.dp)
+                .noRippleClickable { onClick() }
+        ){
+            Text(
+                text = "로그인"
+            )
+        }
+    }
 }
