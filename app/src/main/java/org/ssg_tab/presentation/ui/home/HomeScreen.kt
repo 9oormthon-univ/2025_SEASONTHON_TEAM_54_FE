9fa420e91f659560652e74fc2c34a14329e6bacb
Son Juwan <wndhks171@naver.com>
2025-09-03 12:28:05 +0900

    setting/#13: theme 적용

--- app/src/main/java/org/ssg_tab/presentation/ui/home/HomeScreen.kt
+++ app/src/main/java/org/ssg_tab/presentation/ui/home/HomeScreen.kt
@@ -2,10 +2,16 @@ package org.ssg_tab.presentation.ui.home
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import org.ssg_tab.core.designsystem.theme.SsgTabTheme
 
 @Composable
 fun HomeScreen (
-
+    modifier: Modifier = Modifier
 ) {
-    Text("홈 화면")
+    Text(
+        "홈 화면",
+        color = SsgTabTheme.colors.Black,
+        style = SsgTabTheme.typography.header,
+    )
 }
