bddf8d077ebd80f60375bccb844ffd2483f6150d
Son Juwan <wndhks171@naver.com>
2025-09-03 12:27:52 +0900

    setting/#13: design_system 세팅

--- app/src/main/java/org/ssg_tab/core/designsystem/theme/Color.kt
+++ app/src/main/java/org/ssg_tab/core/designsystem/theme/Color.kt
@@ -1,11 +1,43 @@
 package org.ssg_tab.core.designsystem.theme
 
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val TextBlue = Color(0xFF0073F7)
+val MainBlue = Color(0xFF4699F8)
+val SubBlue = Color(0xFF57B5F9)
+val LightBlue = Color(0xFFEFF9FE)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Black = Color(0xFF1A1A1A)
+val DarkGray = Color(0xFF28303F)
+val MidGray = Color(0xFF5F6474)
+val LightGray = Color(0xFFDDE0E8)
+val White = Color(0xFFEFF9FE)
+
+@Immutable
+data class SsgTabColors(
+    val TextBlue: Color,
+    val MainBlue: Color,
+    val SubBlue: Color,
+    val LightBlue: Color,
+    val Black: Color,
+    val DarkGray: Color,
+    val MidGray: Color,
+    val LightGray: Color,
+    val White: Color,
+)
+
+val defaultSsgTabColors = SsgTabColors(
+    TextBlue = TextBlue,
+    MainBlue = MainBlue,
+    SubBlue = SubBlue,
+    LightBlue = LightBlue,
+    Black = Black,
+    DarkGray = DarkGray,
+    MidGray = MidGray,
+    LightGray = LightGray,
+    White = White,
+)
+
+val LocalSsgTabColors = staticCompositionLocalOf { defaultSsgTabColors }
