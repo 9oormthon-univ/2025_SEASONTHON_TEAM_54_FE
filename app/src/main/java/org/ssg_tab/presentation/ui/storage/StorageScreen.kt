fb3f401c379418e6faf6805ceb21dae5710f1f91
Son Juwan <wndhks171@naver.com>
2025-09-05 01:15:23 +0900

    feat/#21: 보관함 뷰 작업

--- app/src/main/java/org/ssg_tab/presentation/ui/storage/StorageScreen.kt
+++ app/src/main/java/org/ssg_tab/presentation/ui/storage/StorageScreen.kt
@@ -1,11 +1,155 @@
 package org.ssg_tab.presentation.ui.storage
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.ssg_tab.R
+import org.ssg_tab.core.designsystem.component.SsgTabTopBar
+import org.ssg_tab.core.designsystem.theme.SsgTabTheme
+import org.ssg_tab.presentation.ui.storage.component.StorageCategoryChip
+import org.ssg_tab.presentation.ui.storage.component.StorageGridItem
+import org.ssg_tab.presentation.ui.storage.component.StorageGridItemFolder
+import org.ssg_tab.presentation.ui.storage.component.StorageTabButton
+import org.ssg_tab.presentation.ui.storage.component.StorageTimeTabButton
+import org.ssg_tab.presentation.ui.storage.component.TimeTabType
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewStorageScreen() {
+    SsgTabTheme {
+        StorageScreen(
+            name = List(15) { GridItem(name = "항목 $it") },
+            time = List(6) { GridWeek(time = "2024년 ${it + 1}분기") }
+        )
+    }
+}
 
 @Composable
-fun StorageScreen (
-
+fun StorageScreen(
+    name: List<GridItem>,
+    time: List<GridWeek>,
+    modifier: Modifier = Modifier,
 ) {
-    Text("보관함 화면")
+    val categories = listOf("전체", "주식", "취업", "청약", "부동산", "암호화폐", "해외투자")
+    var selectedCategory by remember { mutableStateOf("전체") }
+    var selectedSortOption by remember { mutableStateOf("최신순") }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = SsgTabTheme.colors.White),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        SsgTabTopBar(
+            leftIcon = R.drawable.ic_core_save,
+            middleText = "내 관심목록",
+            rightIcon = R.drawable.ic_core_search,
+        )
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        StorageTabButton(
+            options = listOf("최신순", "분기별"),
+            selectedOption = selectedSortOption,
+            onOptionSelected = { selectedSortOption = it }
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        when (selectedSortOption) {
+            "최신순" -> {
+                LazyRow(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 20.dp),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    items(categories) { category ->
+                        StorageCategoryChip(
+                            content = category,
+                            isEnabled = selectedCategory == category,
+                            onClick = { selectedCategory = category }
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(14.dp))
+
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(3),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp),
+                    horizontalArrangement = Arrangement.spacedBy(12.dp),
+                    verticalArrangement = Arrangement.spacedBy(9.dp),
+                ) {
+                    itemsIndexed(
+                        items = name,
+                        key = { _, item -> item.name }
+                    ) { _, item ->
+                        StorageGridItem(
+                            title = "${item.name}",
+                            imageUrl = "",
+                            isLiked = true
+                        )
+                    }
+                }
+            }
+
+            "분기별" -> {
+                StorageTimeTabButton(
+                    selectedTab = TimeTabType.WEEK,
+                    onTabSelected =
+                        { }
+                )
+                Spacer(modifier = Modifier.height(14.dp))
+
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(3),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp),
+                    horizontalArrangement = Arrangement.spacedBy(12.dp),
+                    verticalArrangement = Arrangement.spacedBy(9.dp),
+                ) {
+                    itemsIndexed(
+                        items = time,
+                        key = { _, item -> item.time }
+                    ) { _, item ->
+                        StorageGridItemFolder(
+                            folderName = "${item.time}",
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
+
+data class GridItem(
+    val name: String,
+)
+
+data class GridWeek(
+    val time: String,
+)
--- app/src/main/java/org/ssg_tab/presentation/ui/storage/navigation/StorageNavigation.kt
+++ app/src/main/java/org/ssg_tab/presentation/ui/storage/navigation/StorageNavigation.kt
@@ -8,21 +8,66 @@ import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import kotlinx.serialization.Serializable
 import org.ssg_tab.core.navigation.MainTabRoute
+import org.ssg_tab.presentation.ui.storage.GridItem
+import org.ssg_tab.presentation.ui.storage.GridWeek
 import org.ssg_tab.presentation.ui.storage.StorageScreen
 
 fun NavController.navigateToStorage(
-    navOptions: NavOptions? = null
+    navOptions: NavOptions? = null,
 ) {
     navigate(Storage, navOptions)
 }
 
 fun NavGraphBuilder.storageNavigation(
     paddingValues: PaddingValues,
-    snackbarHostState: SnackbarHostState
+    snackbarHostState: SnackbarHostState,
 ) {
     composable<Storage> {
-        StorageScreen()
+        StorageScreen(
+            name = getMockGridItems(),
+            time = getMockTimeItems()
+        )
     }
+}
+
+// 임시 데이터 입니다
+private fun getMockGridItems(): List<GridItem> {
+    return listOf(
+        GridItem(name = "삼성전자 주식 분석"),
+        GridItem(name = "네이버 신입 채용"),
+        GridItem(name = "강남 아파트 매매"),
+        GridItem(name = "카카오 인턴십"),
+        GridItem(name = "SK하이닉스 주가"),
+        GridItem(name = "판교 원룸 전세"),
+        GridItem(name = "LG화학 채용공고"),
+        GridItem(name = "현대차 주식"),
+        GridItem(name = "스타트업 취업"),
+        GridItem(name = "서초구 오피스텔"),
+        GridItem(name = "POSCO 신입"),
+        GridItem(name = "셀트리온 바이오"),
+        GridItem(name = "분당 신축 아파트"),
+        GridItem(name = "넷플릭스 한국지사"),
+        GridItem(name = "비트코인 투자"),
+        GridItem(name = "테슬라 주식 전망"),
+        GridItem(name = "구글 코리아 채용"),
+        GridItem(name = "강서구 주택"),
+        GridItem(name = "애플 신제품"),
+        GridItem(name = "마이크로소프트"),
+        GridItem(name = "부산 부동산"),
+        GridItem(name = "스마트팜 투자"),
+        GridItem(name = "메타버스 기업"),
+        GridItem(name = "ESG 펀드"),
+    )
+}
+
+private fun getMockTimeItems(): List<GridWeek> {
+    return listOf(
+        GridWeek(time = "1주차"),
+        GridWeek(time = "2주차"),
+        GridWeek(time = "3주차"),
+        GridWeek(time = "4주차"),
+
+        )
 }
 
 @Serializable
