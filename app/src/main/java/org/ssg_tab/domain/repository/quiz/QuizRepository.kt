478906c1819c05887f8b8cdb5069f815d5c5bdab
Son Juwan <wndhks171@naver.com>
2025-09-06 00:57:15 +0900

    feat/#37: domain/quiz api 파일 추가

---
+++ app/src/main/java/org/ssg_tab/domain/repository/quiz/QuizRepository.kt
@@ -0,0 +1,7 @@
+package org.ssg_tab.domain.repository.quiz
+
+import org.ssg_tab.domain.model.entity.QuizEntity
+
+interface QuizRepository {
+    suspend fun getQuizList(categoryId: Int, difficulty: String): Result<List<QuizEntity>>
+}
