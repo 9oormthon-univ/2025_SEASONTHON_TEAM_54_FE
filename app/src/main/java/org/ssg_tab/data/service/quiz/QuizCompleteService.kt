3b9e75bf885223221b60e21add9bd32db2344609
Son Juwan <wndhks171@naver.com>
2025-09-07 04:53:34 +0900

    feat/#69: data/quiz api

--- app/src/main/java/org/ssg_tab/data/service/quiz/QuizCompleteService.kt
+++ app/src/main/java/org/ssg_tab/data/service/quiz/QuizCompleteService.kt
@@ -3,11 +3,13 @@ package org.ssg_tab.data.service.quiz
 import org.ssg_tab.data.dto.request.quiz.QuizCompleteRequestDto
 import org.ssg_tab.data.dto.response.base.BaseResponse
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface QuizCompleteService {
     @POST ("/quiz")
     suspend fun completeQuiz(
+        @Header("Authorization") accessToken: String,
         @Body quizCompleteRequestDto: QuizCompleteRequestDto
     ):BaseResponse<Unit>
 }
--- app/src/main/java/org/ssg_tab/data/service/quiz/QuizService.kt
+++ app/src/main/java/org/ssg_tab/data/service/quiz/QuizService.kt
@@ -3,12 +3,14 @@ package org.ssg_tab.data.service.quiz
 import org.ssg_tab.data.dto.response.base.BaseResponse
 import org.ssg_tab.data.dto.response.quiz.QuizListResponseDto
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Query
 
 
 interface QuizService {
     @GET("/quiz")
     suspend fun getQuizList(
+        @Header("Authorization") accessToken: String,
         @Query("categoryId") categoryId: Int,
         @Query("difficulty") difficulty: String,
     ): BaseResponse<QuizListResponseDto>
